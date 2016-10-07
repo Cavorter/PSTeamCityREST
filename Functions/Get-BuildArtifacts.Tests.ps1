73a1a39119b3052e1e59948c5095ebd0f12ca795
Nathan Stohlmann <nathan@virtualmicropolis.com>
2016-10-07 08:50:57 -0500

    New Function: Get-BuildArtifacts. Retrieves artifact information for a specified build id.

---
+++ Functions/Get-BuildArtifacts.Tests.ps1
@@ -0,0 +1,41 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-BuildArtifacts" {
+	Mock -Command Invoke-RestMethod -With { return $true }
+
+	Context "Parameter Validation" {
+		$serverVal = "http://not.a.real.server.url"
+		$credentialVal = New-Object pscredential("user", ( ConvertTo-SecureString -String "Password" -AsPlainText -Force ))
+		$buildVal = "1234"
+		
+		It "Processes Server Parameter" {
+			Get-BuildArtifacts -Server $serverVal -Build $buildVal
+			Assert-MockCalled -Command Invoke-RestMethod -ParameterFilter { $Uri -like "$serverVal*" } -Scope It
+		}
+		
+		It "Throws if the Server parameter is missing or not specified" {
+			{ Get-BuildArtifacts  -Build $buildVal -Server } | Should Throw
+		}
+		
+		It "Processes Credential Parameter" {
+			Get-BuildArtifacts -Server $serverVal -Credential $credentialVal -Build $buildVal
+			Assert-MockCalled -Command Invoke-RestMethod -ParameterFilter { $Credential -eq $credentialVal	} -Scope It
+		}
+		
+		It "Ignores Credential parameter if not supplied" {
+			Get-BuildArtifacts -Server $serverVal -Build $buildVal
+			Assert-MockCalled -Command Invoke-RestMethod -ParameterFilter { !($Credential) }
+		}
+		
+		It "Processes Build parameter" {
+			Get-BuildArtifacts -Server $serverVal -Build $buildVal
+			Assert-MockCalled -Command Invoke-RestMethod -ParameterFilter { $Uri -like "*$buildVal*" }
+		}
+		
+		It "Throws if the Build parameter is missing or not specified" {
+			{ Get-BuildArtifacts -Server $serverVal -Build } | Should Throw
+		}
+	}
+}
---
+++ Functions/Get-BuildArtifacts.ps1
@@ -0,0 +1,33 @@
+﻿function Get-BuildArtifacts {
+	<#
+		.SYNOPSIS
+			Gets a list of artifacts from a particular TeamCity build
+		.DESCRIPTION
+			Retrieves a list of the file artifacts for a build with a specific ID from a TeamCity server.
+		.PARAMETER Server
+			URL of the TeamCity server.
+			
+		.PARAMETER Credential
+			Login credentials for the TeamCity server. Credentials are required unless the server administrator has enabled Guest account access.
+			
+		.PARAMETER Build
+			The buildId of the executed build of a configuration
+	#>
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Server,
+		
+		[pscredential]$Credential,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$Build
+	)
+	$headers = @{ Accept = 'application/json'}
+	$uri = "$Server/httpAuth/app/rest/builds/id:$Build/artifacts/children/"
+	if ( $Credential ) {
+		$result = Invoke-RestMethod -Headers $headers -Uri $uri -Credential $Credential
+	} else {
+		$result = Invoke-RestMethod -Headers $headers -Uri $uri
+	}
+	Write-Output $result
+}
