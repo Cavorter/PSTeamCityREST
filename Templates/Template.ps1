4f273d1ba730f9ee6dcc47c67ddf922b6fda9202
Nathan Stohlmann <nathan@virtualmicropolis.com>
2016-10-17 22:22:11 -0500

    New function and test templates to help stand up new functions instead of the default Pester templates.

---
+++ Templates/Template.ps1
@@ -0,0 +1,34 @@
+﻿function Get-TCProjects {
+	<#
+		.SYOPSIS
+			Retrieves a list of Projects from the specified TeamCity server
+		
+		.DESCRIPTION
+			This function requests the complete list of projects from the TeamCity server through the REST endpoint at http://url/httpAuth/app/rest/projects
+			
+			The list of projects returned is limited to those that the supplied credentials has access to.
+			
+		.PARAMETER Server
+			URL of the TeamCity server.
+			
+		.PARAMETER Credential
+			Login credentials for the TeamCity server. Credentials are required unless the server administrator has enabled Guest account access.
+	#>
+	
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Server,
+		
+		[pscredential]$Credential
+	)
+	
+	$headers = @{ Accept = 'application/json'}
+	$uri = "$Server/httpAuth/app/rest/projects"
+	if ( $Credential ) {
+		$result = Invoke-RestMethod -Headers $headers -Uri $uri -Credential $Credential
+	} else {
+		$result = Invoke-RestMethod -Headers $headers -Uri $uri
+	}
+	Write-Output $result.project
+}
